31d27f6b0917565c9754abbfb39ddfe95b1860ce
Avery <aagles@princeton.edu>
2020-07-29 12:21:10 -0400

    now alginator.py can take inputs like 'GGGG'

--- alginator.py
+++ alginator.py
@@ -61,63 +61,70 @@ mon_pos = 1 #initializing position of monomer in the polymer
 for mon in polymer:
     #beginning of polymer
     if mon_pos == 1:
-        if mon == G:
-            structure = '../structures/GNRD.csv'
-        if mon == M:
-            structure = '../structures/MNRD.csv'
+        if mon == 'G':
+            structure = './structures/GNRD.csv'
+        if mon == 'M':
+            structure = './structures/MNRD.csv'
     #end of polymer
     elif mon_pos == len(polymer):
-        if mon == G:
-            structure = '../structures/GRED.csv'
-        if mon == M:
-            structure = '../structures/MRED.csv'
+        if mon == 'G':
+            structure = './structures/GRED.csv'
+        if mon == 'M':
+            structure = './structures/MRED.csv'
+    elif mon_pos % 2 == 0:
+        if mon == 'G':
+            structure = './structures/GEVN.csv'
+        if mon == 'M':
+            structure = './structures/MEVN.csv'
     else:
-        if
+        if mon == 'G':
+            structure = './structures/GODD.csv'
+        if mon == 'M':
+            structure = './structures/MODD.csv'
 
-structure = './GRED.csv'
+    ## Grabbing info in structure and appending to .csv
+    with open(structure, 'r') as f:
+        dict_reader = DictReader(f)
+        resNum += 1
 
-with open(structure, 'r') as f:
-    dict_reader = DictReader(f)
-    resNum += 1
+        # loopin through lines of data in csv
+        move = new_move #resetting the translation for this monomer
+        for row in dict_reader:
+            atomName = row['name']
+            atomNum += 1 #index for atom number
+            resName = row['res']
+            elem = row['element']
+            x = float('%.3f'%(float(row['x']) + move[0])) #have to convert to number before addition
+            y = float('%.3f'%(float(row['y']) + move[1]))
+            z = float('%.3f'%(float(row['z']) + move[2]))
+            occupancy = '1.00'
+            tempFact = '0.00'
+            none = "" #for columns in pdb that will be empty
 
+            atomName = atomName #left justified, 4 columns
+            # atomNum
+            altLoc = none.rjust(0)
+            resName = resName.rjust(4) #right adjusted, 4 columns (ASSUMING 4 characters)
+            chainID = none.rjust(0)
+            # resNum
+            code = none.rjust(1)
+            x = str(x).rjust(8) #convert back to string
+            y = str(y).rjust(8)
+            z = str(z).rjust(8)
+            occupancy = occupancy.rjust(6)
+            tempFact = tempFact.rjust(6)
+            segID = none.ljust(4)
+            elem = elem.rjust(2)
 
-    # loopin through lines of data in csv
-    move = new_move #resetting the translation for this monomer
-    for row in dict_reader:
-        atomName = row['name']
-        atomNum += 1 #index for atom number
-        resName = row['res']
-        elem = row['element']
-        x = float(row['x']) + move[0] #have to convert to number before addition
-        y = float(row['y']) + move[1]
-        z = float(row['z']) + move[2]
-        occupancy = '1.00'
-        tempFact = '0.00'
-        none = "" #for columns in pdb that will be empty
+            # This final step redefines the move for the next monomer
+            if atomName == 'O1':
+                new_move = [float(row['x']), float(row['y']), float(row['z'])]
+                #print(new_move)
+            #print(str(resNum).rjust(4))
 
-        atomName = atomName.ljust(4) #left justified, 4 columns
-        # atomNum
-        altLoc = none.rjust(0)
-        resName = resName.rjust(4) #right adjusted, 4 columns (ASSUMING 4 characters)
-        chainID = none.rjust(0)
-        # resNum
-        code = none.rjust(1)
-        x = str(x).rjust(8) #convert back to string
-        y = str(y).rjust(8)
-        z = str(z).rjust(8)
-        occupancy = occupancy.rjust(6)
-        tempFact = tempFact.rjust(6)
-        segID = none.ljust(4)
-        elem = elem.rjust(2)
-
-        # This final step redefines the move for the next monomer
-        if atomName == 'O1':
-            new_move = [row['x'], row['y'], row['z']]
-
-        #print(str(resNum).rjust(4))
-
-        with open(output_name, 'a') as output: #a for append!
-            output.write("ATOM  " + str(atomNum).rjust(5) + none.rjust(2) + atomName + altLoc + resName + " " + chainID + str(resNum).rjust(4) + code + none.rjust(3) + x + y + z + occupancy + tempFact + none.rjust(6) + segID + elem + '\n')
+            with open(output_name, 'a') as output: #a for append!
+                output.write("ATOM  " + str(atomNum).rjust(5) + none.rjust(2) + atomName.ljust(4) + altLoc + resName + " " + chainID + str(resNum).rjust(4) + code + none.rjust(3) + x + y + z + occupancy + tempFact + none.rjust(6) + segID + elem + '\n')
+    mon_pos += 1
 
 with open(output_name, 'a') as output:
     output.write("TER\nENDMDL")
